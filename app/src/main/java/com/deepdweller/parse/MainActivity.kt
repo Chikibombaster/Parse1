d07900364e34dcaf498b831952481305abba05a9
mariklolik <marshelo44@gmail.com>
2021-01-18 16:43:14 +0700

    New Parse

--- app/src/main/java/com/deepdweller/parse/MainActivity.kt
+++ app/src/main/java/com/deepdweller/parse/MainActivity.kt
@@ -1,37 +1,29 @@
 package com.deepdweller.parse
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.deepdweller.parse.R
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
-import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 import java.io.IOException
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
-    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "KotlinApp"
-        textView = findViewById(R.id.textView)
-        button = findViewById(R.id.btnParseHTML)
-        button.setOnClickListener {
-            getHtmlFromWeb()
-        }
     }
-    private fun getHtmlFromWeb() {
+
+    fun parse(view: View){
         Thread(Runnable {
             val stringBuilder = StringBuilder()
             try {
                 val doc: Document = Jsoup.connect("https://yandex.ru/pogoda/barnaul").get()
+                val temp: String = doc.select("div.temp.fact__temp.fact__temp_size_s > span.temp__value").text()
                 val title: String = doc.title()
-                val links: Elements = doc.select("div.weather-table_temp")
-                stringBuilder.append(title).append("\n")
-                for (link in links) {
-                    stringBuilder.append("\n").append("Link :").append(link.attr("weather-table_temp")).append("\n").append("Text : ").append(link.text())
-                }
+                stringBuilder.append(title).append("\n").append(temp)
             } catch (e: IOException) {
                 stringBuilder.append("Error : ").append(e.message).append("\n")
             }
